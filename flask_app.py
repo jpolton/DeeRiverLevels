fb1e83153eea10f55fd79f46f9e9aae14f7f11fc
jpolton <22616872+jpolton@users.noreply.github.com>
2026-02-10 21:36:50 +0000

    update queens park to EA

--- flask_app.py
+++ flask_app.py
@@ -55,7 +55,7 @@ STATIONS = {
         "source": "NRW",
         "description": "Natural Resources Wales monitoring station",
         "parameter_id": 41
-        # Addtional, EA, data on IronBridge: https://environment.data.gov.uk/flood-monitoring/id/stations/067027_TG_127/stageScale
+        # Addtional, EA, data on IronBridge (doesn't seem to work): https://environment.data.gov.uk/flood-monitoring/id/stations/067027_TG_127/stageScale
     },
     "farndon": {
         "id": "4170", #NRW:4170 Shoothill:972,
@@ -65,10 +65,10 @@ STATIONS = {
         "parameter_id": 40
     },
     "queens_park": {
-        "id": "10831",
+        "id": "SJ46_109", #EA:SJ46_109 Shoothill:10831,
         "name": "Queens Park ground water level",
-        "source": "Shoothill",
-        "description": "Shoothill monitoring station"
+        "source": "EA",
+        "description": "Environment Agency monitoring station"
     }
 }
 # Extra EA data on Finchett's Gutter etc: https://environment.data.gov.uk/flood-monitoring/id/stations?lat=53.1899&long=-2.8853&dist=10
